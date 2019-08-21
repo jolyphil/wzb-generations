6f9f647d543851ccf78481514b3a4adab8872de3
marcus <marcus@mspittler.eu>
2019-08-21 10:42:22 +0200

    Graphs prettified

--- scripts/generate_graphs.R
+++ scripts/generate_graphs.R
@@ -7,6 +7,7 @@ library(tibble) # Dataframes
 library(rlang)
 library(dplyr) # Data wrangling
 library(ggplot2) # Graphs
+library(scales)
 
 # ______________________________________________________________________________
 # Load data ====
@@ -50,45 +51,67 @@ varname <- c("polintr",
              "pbldmn",
              "bctprd",
              "clsprty")
-label <- c("Interested in politics (proportion)",
-           "Voted in last national election (proportion)",
-           "Contacted politician or government official, last 12 months (proportion)",
-           "Worked in political party or action group, last 12 months (proportion)",
-           "Worked in another organisation or association, last 12 months (proportion)",
-           "Worn or displayed campaign badge/sticker, last 12 months (proportion)",
-           "Signed petition, last 12 months (proportion)",
-           "Taken part in lawful public demonstration, last 12 months (proportion)",
-           "Boycotted certain products, last 12 months(proportion)",
-           "Feel closer to a particular party (proportion)"
+label <- c("Interested in politics",
+           "Voted in last national election",
+           "Contacted politician or government official, in last 12 months",
+           "Worked in political party or action group, in last 12 months",
+           "Worked in another organisation or association, in last 12 months",
+           "Worn or displayed campaign badge/sticker, in last 12 months",
+           "Signed a petition, in last 12 months",
+           "Taken part in lawful public demonstration, in last 12 months",
+           "Boycotted certain products, in last 12 months",
+           "Feel close to a particular party"
            )
 
 # _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 # Loop over variables and export graphs ----
 
-for(i in seq_along(varname)) {
+plot_regular_freq <- function(.data, varname , label){
   
-  p <- ggplot(ess_gr, aes(x = year,
-                          y = ess_gr[[varname[i]]], 
-                          group = generation)) +
-    geom_line(aes(color = generation)) +
-    geom_point(aes(color = generation)) +
-    scale_x_continuous(name = "Year") +
-    scale_y_continuous(name = label[i]) + 
-    scale_color_discrete(name="",
-                         labels = c("Baby boomers (1955-69)", 
-                                    "Generation X (1970-84)", 
-                                    "Millennials (1985-...)"))
+  gd <- .data %>% mutate(var = !!varname)
   
-  filepath <- paste0("figures/", varname[i], ".png")
-  ggsave(filepath, p)
-  filepath <- paste0("figures/", varname[i], ".pdf")
-  ggsave(filepath, p)
+  gd %>%     
+    ggplot(
+      aes(x = year, y = var*100,
+      group = generation)) +
+    geom_line(aes(color = generation), size = 1.05) +
+    geom_point(aes(color = generation), size = 3) +
+    scale_x_continuous(
+      name = NULL,
+      limits = c(2003,2016),
+      breaks = c(2003, seq(2004,2016,2))
+    ) +
+    scale_y_continuous(
+      name = "Share in generation (in %)", limits = c(0, ceiling(max(gd$var)*100)),
+      breaks=pretty_breaks()
+    ) +
+    scale_color_manual(
+      values = rev(wesanderson::wes_palette("Darjeeling1",3)),
+      labels = c("Baby boomers (1955-69)", "Generation X (1970-84)", "Millennials (1985-2000)")
+    ) +
+    labs(title = label, subtitle = "European Social Survey (2003-2016)", color = "Generation:") +
+    theme_bw() +
+    theme(
+      text = element_text(family = "Crimson", size = 12),
+      plot.subtitle = element_text(face = "italic"),
+      legend.position = "bottom"
+    )
+
+  ggsave(
+    paste0("figures/", quo_name(varname),".png"),
+    width = 19, height = 19/1.35,
+    dpi = 300, units = "cm"
+  )
+  #filepath <- paste0("figures/", varname[i], ".pdf")
+  #ggsave(filepath, p)
 }
+
+purrr::walk2(varname, label, ~plot_regular_freq(ess_gr, parse_quo(.x, current_env()), .y))
 
 # _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 # Compute and plot "Age of Interview" graphs ----
 
-plot_agedoi_freq <- function(.data, value, fname = "agedoi"){
+plot_agedoi_freq <- function(.data, value, label, fname = "agedoi"){
   values <- enquo(value)
   
   .data %>% 
@@ -96,32 +119,54 @@ plot_agedoi_freq <- function(.data, value, fname = "agedoi"){
     # Compute weighted means
     group_by(generation, age_doi) %>% 
     count(!!values, wt = dweight) %>%
-    filter(!is.na(!!values)) %>% 
-    mutate(f = n/sum(n)) %>% 
+    filter(!is.na(!!values)) %>%
+    mutate(f = n/sum(n)*100) %>%
     # Remove when too few cases
     filter(!(sum(n) < 30)) %>%
     # Clean
-    filter(!!values == 1) %>% 
+    filter(!!values == 1) %>%
     select(-n, -!!values) %>% 
     # Generate plot
     ggplot(aes(
       x = age_doi, y = f,
-      group = generation, color = generation
-    )) + 
+      group = rev(generation), color = rev(generation)
+    )) +
     geom_point() +
-    geom_smooth(method = 'loess', formula = 'y ~ x') +
-    labs(x = "Age at date of interview",
-         y = tibble(varname, label) %>% 
-           filter(varname == quo_name(values)) %>% .$label
+    geom_smooth(method = 'loess', formula = 'y ~ x', se = FALSE) +
+    scale_x_continuous(breaks = c(16,seq(20,60,10))) +
+    scale_y_continuous(
+      limits = c(0, NA),
+      breaks = pretty_breaks()
     ) +
-    scale_x_continuous()
-  
-    ggsave(paste0("./figures/",fname ,"-",quo_name(values),".png"))
+    scale_color_discrete(
+      labels = c("Millennials (1985-2000)", 
+                 "Generation X (1970-84)",
+                 "Boomers (1955-69)")
+    ) +
+    coord_cartesian(xlim = c(16,60), expand = TRUE) +
+    xlab("Age at date of interview") + ylab("Share in generation (in %)") +
+    labs(
+      title = label, subtitle = "European Social Survey (2003-2016)",
+      color = "Generation:"
+    ) +
+    theme_bw() +
+    theme(
+      text = element_text(family = "Crimson", size = 12),
+      plot.subtitle = element_text(face = "italic"),
+      legend.position = "bottom"
+    )
+
+  ggsave(
+    paste0("figures/", fname, "-", quo_name(values),".png"),
+    width = 19, height = 19/1.35,
+    dpi = 300, units = "cm"
+  )
 }
 
-c("polintr", "vote", "contplt", "wrkprty", "wrkorg", "badge", "sgnptit",
-  "pbldmn", "bctprd", "clsprty") %>% 
-  purrr::map(~plot_agedoi_freq(ess, !!parse_quo(.x, env = current_env())))
+purrr::walk2(
+  varname, label, 
+  ~plot_agedoi_freq(ess, !!parse_quo(.x, env = current_env()), .y)
+)
 
-plot_agedoi_freq(allbus, polintr, fname = "agedoi-allbus")
-plot_agedoi_freq(allbus, mmbprty, fname = "agedoi-allbus")
+#plot_agedoi_freq(allbus, polintr, fname = "test")
+#plot_agedoi_freq(allbus, mmbprty, fname = "agedoi-allbus")
