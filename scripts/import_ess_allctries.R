90bc3c292a9f6df1f00a431052d319f989a58085
Marcus <gitlab@mspittler.eu>
2020-07-06 14:48:59 +0200

    Improved import_ess_allctries to import ESS9 data as well

--- scripts/import_ess_allctries.R
+++ scripts/import_ess_allctries.R
@@ -1,10 +1,27 @@
 library(tidyverse) # Used for data wrangling
 library(essurvey) # Downloads main ESS datafiles
 
+# Define countries of interest ------------------------------------------------------
+countries <-  
+  tibble(
+  cntry = c(
+    c("AT", "BE", "DE", "DK", "ES", "FI", "FR", "GB", "GR", "IE", "IT",
+      "NL", "PT", "SE",
+      "CZ", "BG", "HR", "HU", "LT", "PL", "SI", "SK")
+    # EE, LV, and RO removed due to missing data in weights
+  ),
+  western_europe = c(rep(1, 14), rep(0, 8))
+) %>% 
+  mutate(cname_en = countrycode::countrycode(cntry, "iso2c", "country.name"))
+
 # Import all rounds -----------------------------------------------------------------
+
+# Import all rounds (but round 9 is only read with `foreign`, hence treatment)
 a <- import_all_rounds("ess@byom.de") # <-- Replace: your email has to be registred
 
-ownR::getCodebook(a[[1]]) %>% View()
+# Download round 9 and overwrite in original import
+download_rounds(rounds = 9, ess_email = "ess@byom.de", output_dir = "data/temp/", format = "stata")
+a[[9]] <- haven::read_dta("./data/temp/ESS9/ESS9e02.dta", encoding='latin1')
 
 # Limit variables and combine dataset -----------------------------------------------
 b <- 
@@ -16,16 +33,18 @@ b <-
       wrkorg, badge, sgnptit, pbldmn, bctprd, clsprty, lrscale, stfdem, yrbrn, gndr,
       eisced, mnactic, mbtru, domicil, brncntr)
   ) %>% 
-  purrr::map(essurvey::recode_missings) 
+  purrr::map(essurvey::recode_missings) %>% 
+  bind_rows()
 
-# Adjust trust variables in ESS9 from factor to numeric and minus 1
-b <- bind_rows(
-  b[1:8], 
-  b[[9]] %>% 
-    mutate_if(is.factor, as.numeric) %>%
-    mutate_at(vars(matches("trst")), function(x) x -1)
-  )
-
+# Visual check
+b %>% 
+  filter(cntry == "DE") %>% 
+  ggplot() + 
+  geom_density(
+    aes(x=trstprl, group=factor(essround), fill=factor(essround)),
+    alpha=0.5, adjust=2
+  ) + 
+  scale_fill_viridis_d()
 
 # Recode variables ------------------------------------------------------------------
 recode_participation_var <- function(var) {
@@ -63,17 +82,7 @@ c <-
 
 # Reduce country list ---------------------------------------------------------------
 d <-  
-  c %>% right_join(
-  tibble(
-    cntry = c(
-      c("AT", "BE", "DE", "DK", "ES", "FI", "FR", "GB", "GR", "IE", "IT",
-        "NL", "PT", "SE",
-        "CZ", "BG", "HR", "HU", "LT", "PL", "SI", "SK")
-      # EE, LV, and RO removed due to missing data in weights
-    ),
-    western_europe = c(rep(1, 14), rep(0, 8))
-  ) %>% 
-  mutate(cname_en = countrycode::countrycode(cntry, "iso2c", "country.name")),.,
+  c %>% right_join(countries,.,
   by = "cntry") %>% 
   filter(!is.na(cname_en))
 
@@ -81,7 +90,7 @@ d <-
 d %>% group_by(cname_en) %>% count(essround) %>% select(-n) %>% {table(.$essround)}
 
 # Save ------------------------------------------------------------------------------
-d %>% saveRDS(file.path("data", "ess_allctries.rds"))
+d %>% saveRDS(file.path("data", "ess_allctries_19.rds"))
 
 
 
